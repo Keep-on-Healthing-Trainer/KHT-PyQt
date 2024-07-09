5673d5dcee90df29c561f5781c4ad9aad242bee7
silvercong <bin091003@naver.com>
2024-07-09 15:26:25 +0900

    ⚡️ :: socket 연동

--- QR_Page/QR_Page.py
+++ QR_Page/QR_Page.py
@@ -1,9 +1,21 @@
-import sys, qrcode, requests
+import sys, qrcode, requests, asyncio, websockets
+import websocket, rel
+from websocket import create_connection
 from PyQt5.QtWidgets import QApplication, QMainWindow
 from PyQt5.QtGui import QPixmap
 from PyQt5.uic import loadUi
+from urllib.parse import urlparse, parse_qs
 
-url = {url/exercise/qr}
+# url parsing
+def get_query_string(url):
+    response_url = urlparse(url)
+    query_string = response_url.query
+    parse_string = parse_qs(query_string)
+
+    return parse_string
+
+# qr
+url = {url}
 response = requests.get(url)
 qr = qrcode.QRCode(
     version = 1,
@@ -17,6 +29,25 @@ qr.make(fit = True)
 img = qr.make_image(fill_color="black", back_color="white")
 img.save({png})
 
+# sessionId
+parsingData = get_query_string(response.text)
+print(parsingData)
+
+sessionId = parsingData['sessionId'][0]
+print(sessionId)
+
+#async def connect():
+#    async with websockets.connect({url}) as websocket:
+#        data = {
+#            "messageType": "ENTER",
+#            "sessionId": {sessionId},
+#            "senderId": {senderId}
+#        }
+#        await websocket.send(data)
+#        await websocket.send("hello")
+#        websocket_response = await websocket.recv()
+#        print(websocket_response)
+
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
@@ -28,9 +59,18 @@ class MainWindow(QMainWindow):
         pixmap = QPixmap("QR.png")
         self.imageLabel.setPixmap(pixmap)
 
-
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     mainWindow = MainWindow()
     mainWindow.show()
+
+    ws = websocket.create_connection({url})
+    ws.recv()
+    print("Sending 'Hello, World'...")
+    ws.send("Hello, World")
+    print("Sent")
+    print("Receiving...")
+    result = ws.recv()
+    ws.close()
+    # asyncio.get_event_loop().run_until_complete(connect())
     sys.exit(app.exec_())
